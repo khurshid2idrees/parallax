cc9c565c226ac7fc54c5465f2735457654001f49
khurshid2idrees <73442661+khurshid2idrees@users.noreply.github.com>
2023-12-25 09:31:40 +0530

    update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { useSpring, animated } from 'react-spring';
-import { useInView } from 'react-intersection-observer';
+
 
 export default function Header() {
-  const [ref, inView] = useInView();
-
   const slideInUpAnimation = useSpring({
-    opacity: inView ? 1 : 0,
-    transform: inView ? 'translateY(0)' : 'translateY(300px)',
+    from: {
+      opacity: 0,
+      transform: 'translateY(300px)', // Start offscreen
+    },
+    to: {
+      opacity: 1,
+      transform: 'translateY(0)', // Slide in to the normal position
+    },
     config: {
-      duration: 500,
+      duration: 500, // Animation duration in milliseconds
     },
   });
 
   return (
-    <div ref={ref} className='md:ml-28 flex md:mt-20 mt-28'>
+    <div className='md:ml-28 flex md:mt-20 mt-28'>
       <animated.h2 style={slideInUpAnimation} className='font-normal md:text-9xl text-5xl text-white leading-tight font-sans'>
         Our &nbsp;
-        <span id='button' className='border-white border-2 px-8 rounded-full'>Innovative</span>
+        <span id='button' className='border-white border-2 px-8 rounded-full  hover:transition-opacity cursor-pointer'>Innovative</span>
         <br />
         AI
         <br />
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        mainblue:"#1b9bef"
+      }
+    },
   },
   plugins: [],
 }
