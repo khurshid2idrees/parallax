d49aff7bdaaebfd7aef1f19f632db421365fe328
khurshid2idrees <73442661+khurshid2idrees@users.noreply.github.com>
2023-12-25 09:38:08 +0530

    update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import { useSpring, animated } from 'react-spring';
-
+import { useInView } from 'react-intersection-observer';
 
 export default function Header() {
+  const [ref, inView] = useInView();
+
   const slideInUpAnimation = useSpring({
-    from: {
-      opacity: 0,
-      transform: 'translateY(300px)', // Start offscreen
-    },
-    to: {
-      opacity: 1,
-      transform: 'translateY(0)', // Slide in to the normal position
-    },
+    opacity: inView ? 1 : 0,
+    transform: inView ? 'translateY(0)' : 'translateY(300px)',
     config: {
-      duration: 500, // Animation duration in milliseconds
+      duration: 500,
     },
   });
 
   return (
-    <div className='md:ml-28 flex md:mt-20 mt-28'>
+    <div ref={ref} className='md:ml-28 flex md:mt-20 mt-28'>
       <animated.h2 style={slideInUpAnimation} className='font-normal md:text-9xl text-5xl text-white leading-tight font-sans'>
         Our &nbsp;
-        <span id='button' className='border-white border-2 px-8 rounded-full  hover:transition-opacity cursor-pointer'>Innovative</span>
+        <span id='button' className='border-white border-2 px-8 rounded-full'>Innovative</span>
         <br />
         AI
         <br />
